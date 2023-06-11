15ba95bcb74cba35ae32ab9289daed1eed42f911
Joshua <akinleyejoshua.dev@gmail.com>
2023-06-11 06:44:33 +0100

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const baseURL = "http://127.0.0.1:8000/api/v1/facial-expression";
-// const baseURL = "https://dl-web-api-production.up.railway.app/api/v1/facial-expression";
+const baseURL = "https://dl-web-api-production.up.railway.app/api/v1/facial-expression";
 // const baseURL = "https://dl-web-api-flask-production.up.railway.app/api/v1/facial-expression";
 
 const settings = {
